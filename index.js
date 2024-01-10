1c9b53611069729ff48223b65e0170d9c78e36dd
Nino GUEGUEN <ninogueguen@gmail.com>
2024-01-10 11:35:17 +0100

    Projet mouse effect terminé

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+const mouses = document.querySelectorAll(".custom-cursor ");
 
 // Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+window.addEventListener("mousemove", (e) => {
+  mouses.forEach((mouse) => {
+    mouse.style.left = e.clientX + "px";
+    mouse.style.top = e.clientY + "px";
+  });
+});
 
 // S'assurer que les liens sont clickables
 
